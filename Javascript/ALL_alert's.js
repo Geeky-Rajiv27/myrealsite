2a40e3d712f3872ed961abbe41dc0d2ba1de5b1f
rajiv chaudhary <vijarchy@gmail.com>
2025-06-12 22:47:25 +0545

    added lil bit js alerts and made major changes on designs of portfolio section

---
+++ Javascript/ALL_alert's.js
@@ -0,0 +1,24 @@
+function alertDownload()
+{
+    alert("Sorry, currently CV is not available.")
+}
+   /*   <!--------------------( 6th main )------------------------>*/
+   function alertExp()
+   {
+    alert("Yet to gain")
+   }
+  
+   function alertProjects()
+   {
+    alert("Not included all my projects!")
+   }
+
+   function alertHack()
+   {
+    alert("Still working on it!")
+   }
+
+   function alertCompany_work()
+   {
+    alert("Searching for Internship!");
+   }
